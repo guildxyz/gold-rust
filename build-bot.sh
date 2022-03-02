4e5a6c1e97f2437412f96df27f193c523abb5b82
PopcornPaws <lipschitz77@gmail.com>
2022-03-02 15:21:11 +0100

    Keep this Dockerfile.

--- build-bot.sh
+++ build-bot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 DOCKER_BUILDKIT=1 docker build --network host -t "auction-bot" -o bin .
-scp bin/agsol-gold-bot root@95.217.237.232:~/auction-bot/deploy
+#scp bin/agsol-gold-bot root@95.217.237.232:~/auction-bot/deploy
